76b42924b6f3da1f254a6e613ad8b9f0feafc7c6
Bharat16 <bharatbhargava9@gmail.com>
2020-05-25 20:05:53 -0300

    added selecting the movie from the list of appeared movies by click

--- reactapp/src/components/ResultBox.js
+++ reactapp/src/components/ResultBox.js
@@ -4,11 +4,17 @@ class ResultBox extends Component {
     constructor(props) {
         super(props);
         this.state = {}
+        this.onClickDrpDwnItem = this.onClickDrpDwnItem.bind(this);
     }
+
+    onClickDrpDwnItem(s) {
+        this.props.onSelectDrpDwnItem(s);
+    }
+
     render() {
         return (
-            <div className="navbar media bg-white rounded form-group pt-2" style={{ margin: '-15px 0 0 0' }}>
-                <label className="input-lg media-body">{this.props.text}</label>
+            <div onClick={this.onClickDrpDwnItem.bind(this, this.props)} className="navbar media bg-white rounded form-group pt-2" style={{ margin: '-15px 0 0 0' }}>
+                <label className="input-lg media-body" >  {this.props.text}</label>
             </div>
 
         );
