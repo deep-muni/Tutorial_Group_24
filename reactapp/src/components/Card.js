349e5908a70d0a51ca4bffda63e99699ee0fc9da
Deep Muni <deep.muni94@gmail.com>
2020-06-01 19:50:47 -0300

    Tutorial 4: Display all movies

--- reactapp/src/components/Card.js
+++ reactapp/src/components/Card.js
@@ -1,23 +1,29 @@
 import React from "react";
 import './Card.css'
+import Axios from "axios";
 
 export class Card extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            movie: []
+        }
         this.onClickCardView = this.onClickCardView.bind(this);
     }
 
     onClickCardView() {
-        fetch('https://jsonplaceholder.typicode.com/todos/3')
-            .then(response => response.json())
-            .then(json => console.log(json))
+        console.log(this.props.show_id);
+        this.dataRequest("https://api-tutorial4.herokuapp.com/movies?show_id="+ this.props.show_id);
+    }
+
+    async dataRequest(url) {
+        const {data: movie} = await Axios.get(url);
+        this.setState({movie});
     }
     
     render() {
         return (
-            <div className="movie esclas" onClick={this.onClickCardView}>
-                <div className="title">{this.props.title}</div>
-            </div>
+            <p className="movie" onClick={this.onClickCardView}>{this.props.title}</p>
         );
     }
 }
--- reactapp/src/components/Search.js
+++ reactapp/src/components/Search.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./Search.css";
 import Suggestions from "./Suggestions";
 import Card from "./Card";
@@ -7,114 +7,136 @@ import Axios from "axios";
 let userInput = {};
 
 class Search extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      MovieList: [],
-      selectedTup: [],
-      userI: "",
-    };
-    this.suggest = this.suggest.bind(this);
-    this.showSuggestions = this.showSuggestions.bind(this);
-    this.sugClick = this.sugClick.bind(this);
-    this.showCard = this.showCard.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            AllMovies: [],
+            MovieList: [],
+            selectedTup: [],
+            userI: "",
+        };
 
-  async dataRequest(url) {
-    const { data: MovieList } = await Axios.get(url);
-    this.setState({ MovieList });
-  }
-
-  suggest(e) {
-    const userInp = e.target.value;
-    this.setState({
-      userI: userInp,
-    });
-    if (userInp.length > 0) {
-      this.setState({
-        selectedTup: [],
-      });
-      this.dataRequest(
-        "https://api-tutorial4.herokuapp.com/movies?title_like=.*" + userInp
-      );
-      userInput = {
-        borderRadius: "5px 5px 0 0",
-        boxShadow: "inset 0 0 10px 4px #8f8f8f",
-      };
-    } else {
-      this.setState({
-        MovieList: [],
-      });
-      userInput = {};
+        this.suggest = this.suggest.bind(this);
+        this.showSuggestions = this.showSuggestions.bind(this);
+        this.sugClick = this.sugClick.bind(this);
+        this.showCard = this.showCard.bind(this);
     }
-  }
 
-  showSuggestions() {
-    if (this.state.MovieList.length === 0) {
-      return null;
+    async componentDidMount(){
+        const {data : AllMovies} = await Axios.get("https://api-tutorial4.herokuapp.com/movies?");
+        this.setState({AllMovies});
     }
-    return (
-      <div className="listWrapper">
-        {this.state.MovieList.map((tup, index) => {
-          return (
-            <Suggestions
-              onClick={this.sugClick}
-              key={index}
-              title={tup.title}
-            />
-          );
-        })}
-      </div>
-    );
-  }
 
-  sugClick(e) {
-    userInput = {};
-    document.getElementById("userInput").value = e.target.innerText;
-
-    const tup = this.state.MovieList.filter((t) => {
-      return t.title === e.target.innerText;
-    });
-
-    this.setState({
-      MovieList: [],
-      selectedTup: tup,
-    });
-  }
-
-  showCard() {
-    if (this.state.userI.length > 0 && this.state.selectedTup.length > 0) {
-      return (
-        <div>
-          {this.state.selectedTup.map((tup, index) => {
-            return <Card key={index} title={tup.title} />;
-          })}
-        </div>
-      );
-    } else {
-      return null;
+    async dataRequest(url) {
+        const {data: MovieList} = await Axios.get(url);
+        this.setState({MovieList});
     }
-  }
 
-  render() {
-    return (
-      <div className="wrapper">
-        <div className="searchBox">
-          <h2>Movies</h2>
-          <input
-            type="Search"
-            id="userInput"
-            placeholder="Search"
-            className="userInput"
-            style={userInput}
-            onChange={this.suggest}
-          />
-          {this.showSuggestions()}
-        </div>
-        {this.showCard()}
-      </div>
-    );
-  }
+    suggest(e) {
+        const userInp = e.target.value;
+        this.setState({
+            userI: userInp,
+        });
+        if (userInp.length > 0) {
+            this.setState({
+                selectedTup: [],
+            });
+            this.dataRequest(
+                "https://api-tutorial4.herokuapp.com/movies?title_like=.*" + userInp
+            );
+            userInput = {
+                borderRadius: "5px 5px 0 0",
+                boxShadow: "inset 0 0 10px 4px darkcyan",
+            };
+        } else {
+            this.setState({
+                MovieList: [],
+            });
+            userInput = {};
+        }
+    }
+
+    showSuggestions() {
+        if (this.state.MovieList.length === 0) {
+            return null;
+        }
+        return (
+            <div className="listWrapper">
+                {this.state.MovieList.map((tup, index) => {
+                    return (
+                        <Suggestions
+                            onClick={this.sugClick}
+                            key={index}
+                            title={tup.title}
+                        />
+                    );
+                })}
+            </div>
+        );
+    }
+
+    sugClick(e) {
+        userInput = {};
+        document.getElementById("userInput").value = e.target.innerText;
+
+        const tup = this.state.MovieList.filter((t) => {
+            return t.title === e.target.innerText;
+        });
+
+        this.setState({
+            MovieList: [],
+            selectedTup: tup,
+        });
+    }
+
+    showCard() {
+        if (this.state.userI.length > 0 && this.state.selectedTup.length > 0) {
+            return (
+                <div className="card-wrapper">
+                    {this.state.selectedTup.map((tup, index) => {
+                        return <Card key={index} show_id={tup.show_id} title={tup.title}/>;
+                    })}
+                </div>
+            );
+        } else {
+            return null;
+        }
+    }
+
+    initialShow() {
+        if(this.state.selectedTup.length === 0 || this.state.userI.length === 0){
+            return (
+                <div className="card-wrapper">
+                    {this.state.AllMovies.map((tup, index) => {
+                        return <Card key={index} show_id={tup.show_id} title={tup.title}/>;
+                    })}
+                </div>
+            );
+        }
+    }
+
+    render() {
+        return (
+            <div className="wrapper">
+                <div className="searchBox">
+                    <p className="header">Movies</p>
+                    <input
+                        type="Search"
+                        id="userInput"
+                        placeholder="Search"
+                        className="userInput"
+                        style={userInput}
+                        onChange={this.suggest}
+                    />
+                    {this.showSuggestions()}
+                </div>
+                {this.initialShow()}
+                {this.showCard()}
+            </div>
+        );
+    }
+
+
 }
 
 export default Search;
