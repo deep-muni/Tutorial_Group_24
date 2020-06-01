e8eab711fd16abcd39fe3f0c5d6bcf2118716b56
Bharat16 <bharatbhargava9@gmail.com>
2020-06-01 10:08:00 -0300

    Tutorial 4: Adding placeholder to hit api to get movie details on clicking card

--- reactapp/src/components/Card.js
+++ reactapp/src/components/Card.js
@@ -1,11 +1,21 @@
 import React from "react";
 import './Card.css'
 
-export class Card extends React.Component{
+export class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.onClickCardView = this.onClickCardView.bind(this);
+    }
 
+    onClickCardView() {
+        fetch('https://jsonplaceholder.typicode.com/todos/3')
+            .then(response => response.json())
+            .then(json => console.log(json))
+    }
+    
     render() {
         return (
-            <div className="movie">
+            <div className="movie esclas" onClick={this.onClickCardView}>
                 <div className="title">{this.props.title}</div>
             </div>
         );
